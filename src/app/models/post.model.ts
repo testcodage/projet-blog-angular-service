4621006b005b210bdc59859413244f1bd5954d3e
testcodage <56564567+testcodage@users.noreply.github.com>
2019-11-17 19:40:26 +0100

    Delete post.model.ts

--- src/app/models/post.model.ts
+++
@@ -1,9 +0,0 @@
-export class Post {
-  constructor (
-    public index: number,
-    public title: string,
-    public content: string,
-    public date: Date,
-    public loveIts: number
-  ) { }
-}
