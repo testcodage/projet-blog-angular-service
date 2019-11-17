a9945c346dcdde6e223a57d2bc51f9ae4aa51cc8
testcodage <56564567+testcodage@users.noreply.github.com>
2019-11-17 18:29:15 +0100

    Create post.model.ts

---
+++ src/app/models/post.model.ts
@@ -0,0 +1,9 @@
+export class Post {
+  constructor (
+    public index: number,
+    public title: string,
+    public content: string,
+    public date: Date,
+    public loveIts: number
+  ) { }
+}
